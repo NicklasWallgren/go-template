0eb6c2fe1a6057bd1d825132909894fe512524d8
NicklasWallgren <nicklas.wallgren@stim.se>
2022-05-12 11:11:32 +0200

    Pass the correct context to reuse the root tracing span

--- infrastructure/database/mysql.go
+++ infrastructure/database/mysql.go
@@ -2,8 +2,11 @@ package database
 
 import (
 	"fmt"
+	"github.com/go-sql-driver/mysql"
+	sqltrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/database/sql"
 	gormtrace "gopkg.in/DataDog/dd-trace-go.v1/contrib/gorm.io/gorm.v1"
 	mysqlgorm "gorm.io/driver/mysql"
+	"log"
 	"time"
 
 	"github.com/NicklasWallgren/go-template/config"
@@ -34,7 +37,25 @@ func NewDatabase(config *config.AppConfig, logger logger.Logger) Database {
 func connect(url string, logger logger.Logger) (db *gorm.DB, err error) {
 	// The database might not have been initialized yet, retry
 	err = retry.Do(func() (err error) {
-		db, err = gormtrace.Open(mysqlgorm.New(mysqlgorm.Config{DSN: url}), &gorm.Config{Logger: logger.GetGormLogger()})
+
+		// Register augments the provided driver with tracing, enabling it to be loaded by gormtrace.Open.
+		sqltrace.Register("mysql", &mysql.MySQLDriver{}, sqltrace.WithServiceName("go-template"))
+		sqlDb, err := sqltrace.Open("mysql", url)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		db, err = gormtrace.Open(mysqlgorm.New(mysqlgorm.Config{Conn: sqlDb}), &gorm.Config{Logger: logger.GetGormLogger()}, gormtrace.WithServiceName("go-template"))
+		//db, err = gormtrace.Open(mysqlgorm.New(mysqlgorm.Config{DSN: url}), &gorm.Config{Logger: logger.GetGormLogger()}, gormtrace.WithServiceName("go-template"))
+
+		//// Create a root span, giving name, server and resource.
+		//span, _ := tracer.StartSpanFromContext(context.Background(), "my-query",
+		//	tracer.SpanType(ext.SpanTypeSQL),
+		//	tracer.ServiceName("my-db"),
+		//	tracer.ResourceName("initial-access"),
+		//)
+		//defer span.Finish()
+
 		return err
 	}, retry.Delay(200*time.Millisecond), retry.Attempts(10))
 
