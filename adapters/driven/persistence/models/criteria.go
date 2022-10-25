74d33cbd188c304c0f526e3edaadbf8b5039fe82
Nicklas Wallgren <nicklas.wallgren@gmail.com>
2022-10-25 11:08:08 +0200

    Remove Criteria struct in persistence layer

--- adapters/driven/persistence/models/criteria.go
+++ adapters/driven/persistence/models/criteria.go
@@ -1,19 +1,5 @@
 package models
 
-import "github.com/mitchellh/mapstructure"
-
-type Criteria struct {
-	Title string
-}
-
-func (c Criteria) ToWhereMap() (*map[string]interface{}, error) {
-	result := &map[string]interface{}{}
-
-	// TODO, does not support operators
-
-	return result, mapstructure.Decode(c, &result)
-}
-
 type CriteriaAndPagination[T any] struct {
 	Criteria   T
 	Pagination Pagination
